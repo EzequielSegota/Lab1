3e240820e987453e97268c8833164db987f22316
EzequielSegota <36193787+EzequielSegota@users.noreply.github.com>
2019-11-29 10:26:20 -0300

    Add files via upload

---
+++ Segundo Parcial Teorico-Practico/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int id;
+    float coordX;
+    float coordY;
+    char raza[20];
+    char galaxia[20];
+}eAlien;
+
+eAlien* contructorAlien();
+
+eAlien* contructorAlienParametros(int id,float coordX,float coodY,char*,char*);
+
+void setIDAlien(eAlien* this,int id);
+
+void getIDAlien(eAlien* this,int id);
+
+void setCoordX(eAlien* this,float coordX);
+
+void setCoordX(eAlien* this,float coordX);
+
+void getCoordX(eAlien* this,float coordX);
+
+void getCoordY(eAlien* this,float coordY);
+
+void setRaza(eAlien* this,char*);
+
+void setGalaxia(eAlien* this,char*);
+
+//FALTAN GET
+
+
+int escribirAliens (eAlien* Lista,int tam, char* path);
+
+int main()
+{
+    eAlien* alienUno;
+    eAlien* alienDos;
+    FILE* pFile;
+
+    alienUno=contructorAlien();
+
+
+    alienDos=contructorAlienParametros(4,5.2,1.3,"Lalala","Lelele");
+
+    if(alienDos!=NULL)
+        printf("\n%d\t%f\t%f\t%s\t%s",alienDos->id,alienDos->coordX,alienDos->coordY,alienDos->raza,alienDos->galaxia);
+
+
+
+    return 0;
+}
+
+eAlien* contructorAlien()
+{
+    eAlien* returnAux;
+    returnAux = (eAlien*) malloc(sizeof(eAlien));
+    return returnAux;
+}
+
+eAlien* contructorAlienParametros(int id,float coordX,float coorY,char* raza,char* galaxia)
+{
+    eAlien* returnAux;
+    returnAux = (eAlien*) malloc(sizeof(eAlien));
+
+    if(returnAux!=NULL)
+    {
+          returnAux->id=id;
+          returnAux->coordX=coordX;
+          returnAux->coordY=coorY;
+          strcpy(returnAux->galaxia,galaxia);
+          strcpy(returnAux->raza,raza);
+    }
+    return returnAux;
+}
+
+int escribirAliens (eAlien* Lista,int tam, char* path)
+{
+    int i;
+    if(path!=NULL)
+    {
+        path=fopen("Alien.bin","wb");
+
+        for( i=0;i<tam;i++)
+        {
+           //fwrite(,,,);
+        }
+
+        fclose(path);
+    }
+}
